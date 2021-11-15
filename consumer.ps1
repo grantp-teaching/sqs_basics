fdfaec7bf2173cf32d1f3d8f89aedd7537325e91
Peadar Grant <peadargrant@gmail.com>
2021-11-15 16:08:23 +0000

    work from class

---
+++ consumer.ps1
@@ -0,0 +1,22 @@
+﻿# demo SQS consumer script
+# Peadar Grant
+
+$QUrl='https://queue.amazonaws.com/381303118602/testq'
+
+$max_wait = 1
+
+while (1) {
+    $msg_text = (aws sqs receive-message --queue-url $QUrl --wait-time-seconds $max_wait --max-number-of-messages 1)
+    $max_wait = 20
+
+    if ( $msg_text -eq $null) {
+        Write-host "no messages" -ForegroundColor Blue
+        continue
+    }
+
+    $msg = ($msg_text | ConvertFrom-Json).Messages[0]
+
+    Write-Host "Received: " -NoNewline -ForegroundColor Green
+    Write-Host $msg.Body
+    aws sqs delete-message --queue-url $QUrl --receipt-handle $msg.ReceiptHandle
+}
